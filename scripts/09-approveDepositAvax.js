1ec4f42096033fa79180c8daca96ffb4cde7ba87
Clément Aguilar <aguilar.clement@outlook.fr>
2021-05-26 17:36:34 +0200

    Clean up scripts 09 and 10

--- scripts/09-approveDepositAvax.js
+++ scripts/09-approveDepositAvax.js
@@ -1,7 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 
-const { rinkebyProvider, devMnemonicPath, safeReadFile } = require('./const');
+const { fujiProvider, devMnemonicPath, safeReadFile } = require('./const');
 const { WAKITA_TOKEN, AVAX_HANDLER } = require('./bridgeConstants');
 
 const compiledERC20 = require('../cb-sol-cli/chainbridge-solidity/build/contracts/ERC20PresetMinterPauser.json');
@@ -11,7 +11,7 @@ console.log("Dev mnemonic OK:", devMnemonic ? true : false);
 
 const provider = new HDWalletProvider(
     devMnemonic,
-    rinkebyProvider
+    fujiProvider
 );
     
 const web3 = new Web3(provider);
@@ -21,25 +21,28 @@ const erc20 = new web3.eth.Contract(
     WAKITA_TOKEN
 );
 
-const approveDepositDst = async () => {
+const approveDepositAvax = async () => {
     try {
         const accounts = await web3.eth.getAccounts();
     
         console.log('Attempting to approve deposit from the account', accounts[0]);
 
-        await erc20.methods.approve(
+        const tx = await erc20.methods.approve(
                 AVAX_HANDLER, // Recipient, address
-                '100000000000000000000' // Amount, uint256
+                '1000000000000000000000' // Amount, uint256
             )
             .send({
                 from: accounts[0]
             });
-        
+
+        console.log('Tx block number:', tx.blockNumber);
+        console.log('Tx transaction hash:', tx.transactionHash);
+
         console.log('Done!');
     }
     catch(error) {
-        console.error("An error occurred in approveDepositDst():\n", error);
+        console.error("An error occurred in approveDepositAvax():\n", error);
     }
 };
 
-approveDepositDst();
+approveDepositAvax();
--- scripts/10-depositAvax.js
+++ scripts/10-depositAvax.js
@@ -2,8 +2,8 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const ethers = require('ethers');
 
-const { rinkebyProvider, devMnemonicPath, safeReadFile } = require('./const');
-const { AVAX_BRIDGE, SRC_ADDR, AKITA_RESOURCE_ID, AVAX_BRIDGE_FEE } = require('./bridgeConstants');
+const { fujiProvider, devMnemonicPath, safeReadFile } = require('./const');
+const { AVAX_BRIDGE, RECIPIENT_ADDR, AKITA_RESOURCE_ID, AVAX_BRIDGE_FEE, ETH_CHAINID } = require('./bridgeConstants');
 
 const compiledBridge = require('../cb-sol-cli/chainbridge-solidity/build/contracts/Bridge.json');
 
@@ -12,7 +12,7 @@ console.log("Dev mnemonic OK:", devMnemonic != undefined);
 
 const provider = new HDWalletProvider(
     devMnemonic,
-    rinkebyProvider
+    fujiProvider
 );
     
 const web3 = new Web3(provider);
@@ -23,14 +23,13 @@ const bridge = new web3.eth.Contract(
 );
 
 const depositAvax = async () => {
-    const amount = '100000000000000000000'
-    const targetChainId = 0 // 0 is the source chain, 1 is the destination chain, 2+ are other future destination chains
+    const amount = '1000000000000000000000'
 
     // Create the data
     const data = '0x' +
         ethers.utils.hexZeroPad(ethers.BigNumber.from(amount).toHexString(), 32).substr(2) + // Deposit Amount (32 bytes)
-        ethers.utils.hexZeroPad(ethers.utils.hexlify((SRC_ADDR.length - 2)/2), 32).substr(2) + // len(recipientAddress) (32 bytes)
-        SRC_ADDR.substr(2);
+        ethers.utils.hexZeroPad(ethers.utils.hexlify((RECIPIENT_ADDR.length - 2)/2), 32).substr(2) + // len(recipientAddress) (32 bytes)
+        RECIPIENT_ADDR.substr(2);
 
     console.log('Data:', data)
     console.log('Data length:', data.length-2)
@@ -41,7 +40,7 @@ const depositAvax = async () => {
         console.log('Attempting to deposit from the account', accounts[0]);
     
         const tx = await bridge.methods.deposit(
-                targetChainId, // destinationChainID, uint8
+                ETH_CHAINID, // destinationChainID, uint8
                 AKITA_RESOURCE_ID, // resourceID, bytes32
                 data, // data, bytes calldata
             )
