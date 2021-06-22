77b7071eb463e92d0f6d85a364051ce8f14a24a1
Clément Aguilar <aguilar.clement@outlook.fr>
2021-06-22 18:46:51 +0200

    Update Avalanche providers

--- scripts/v2.0/const.js
+++ scripts/v2.0/const.js
@@ -8,10 +8,8 @@ export const IS_PRODUCTION = false
 export const PROVIDER = {
     [CHAIN_ID.ETHEREUM]: 'https://mainnet.infura.io/v3/418e2ad2a59645cab005c2a1712a1873',
     [CHAIN_ID.RINKEBY]: 'https://rinkeby.infura.io/v3/418e2ad2a59645cab005c2a1712a1873',
-    [CHAIN_ID.AVALANCHE]:
-        'https://avalanche--mainnet--rpc.datahub.figment.io/apikey/9dbc9db32d2aa223eec796262c6339b6/ext/bc/C/rpc',
-    [CHAIN_ID.FUJI]:
-        'https://avalanche--fuji--rpc.datahub.figment.io/apikey/9dbc9db32d2aa223eec796262c6339b6/ext/bc/C/rpc'
+    [CHAIN_ID.AVALANCHE]: 'https://api.avax.network/ext/bc/C/rpc',
+    [CHAIN_ID.FUJI]: 'https://api.avax-test.network/ext/bc/C/rpc'
 }
 
 // Danger zone
